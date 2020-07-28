10b32b7583268b36a88900933d88a485b758a7da
Amirhossein Afshinfard <31241012+afshinfard@users.noreply.github.com>
2020-07-28 12:27:13 +0430

    Update iolimits.py

--- physlr/iolimits.py
+++ physlr/iolimits.py
@@ -13,17 +13,13 @@ if __name__ == "__main__":
 	r1_file = gzip.open(r1_file_name, 'r')
 	i1_file_name = args.input_i1
 	i1_file = gzip.open(i1_file_name, 'r')
-	
 	out_file_prefix = args.output_prefix
 	out_file_R1 = gzip.open(out_file_prefix + '_R1_barcoded.fastq.gz', 'wb')
-	
 	r1_header = ''
 	r1_seq = ''
 	r1_orient = ''
 	r1_qual = ''
-	
 	i1_seq = ''
-	
 	cnt = 1	
 	with gzip.open(r1_file_name, 'r') as r1_file:
 	for r1_line in r1_file:
@@ -32,7 +28,7 @@ if __name__ == "__main__":
 			next(i1_file)
     		if cnt==2:
       			r1_seq = r1_line
-      			i1_seq = next(i1_file)
+			i1_seq = next(i1_file)
     		if cnt==3:
       			r1_orient = r1_line
 			next(i1_file)
@@ -42,6 +38,5 @@ if __name__ == "__main__":
 			out_4line = r1_header + b'_' + i1_seq + r1_seq + r1_orient + r1_qual
 			out_file_R1.write(out_byte)
 		cnt += 1
-	
 	i1_file.close()
 	out_file_R1.close()
